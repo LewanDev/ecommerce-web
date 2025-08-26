682442e746e111bd2d368058aaed4def6ad1beab
lewan87 <nawemarchelli@gmail.com>
2025-08-26 17:30:56 -0300

    first commit / add product

---
+++ src/app/add-product/page.tsx
@@ -0,0 +1,64 @@
+import { prisma } from "@/lib/db/prisma";
+import { redirect } from "next/navigation";
+
+export const metadata = {
+  title: "Agregar producto - TiendaOnline",
+};
+
+async function addProduct(formData: FormData) {
+  "use server";
+
+  const name = formData.get("name")?.toString();
+  const description = formData.get("description")?.toString();
+  const imageUrl = formData.get("imageUrl")?.toString();
+  const price = Number(formData.get("price") || 0);
+
+  if (!name || !description || !imageUrl || !price) {
+    throw Error("Faltan campos requeridos");
+  }
+
+  await prisma.product.create({
+    data: { name, description, imageUrl, price },
+  });
+
+  redirect("/");
+}
+
+export default function AddProductPage() {
+  return (
+    <div className="flex flex-col">
+      <h1 className="text-2xl m-3 font-bold">Agregar producto</h1>
+      <form className="flex flex-col gap-5">
+        <input
+          required
+          name="name"
+          placeholder="Nombre"
+          className="input input-bordered w-full"
+        />
+        <textarea
+          required
+          name="description"
+          placeholder="Descripción"
+          className="textarea textarea-bordered w-full"
+        ></textarea>
+        <input
+          required
+          name="imageUrl"
+          placeholder="URL de Imagen"
+          type="url"
+          className="input input-bordered w-full"
+        />
+        <input
+          required
+          name="price"
+          placeholder="Precio"
+          type="number"
+          className="input input-bordered w-full"
+        />
+        <button type="submit" className="btn btn-primary btn-block">
+          Agregar
+        </button>
+      </form>
+    </div>
+  );
+}
