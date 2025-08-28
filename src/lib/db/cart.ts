1602d829bb94b06103309fda132d0b6c012871ab
lewan87 <nawemarchelli@gmail.com>
2025-08-28 17:39:30 -0300

    added cart to product

---
+++ src/lib/db/cart.ts
@@ -0,0 +1,51 @@
+import { cookies } from "next/headers";
+import { prisma } from "./prisma";
+import { Cart, Prisma } from "@prisma/client";
+
+export type CartWithProducts = Prisma.CartGetPayload<{
+  include: { items: { include: { product: true } } };
+}>;
+
+export type ShoppingCart = CartWithProducts & {
+  size: number;
+  subtotal: number;
+};
+
+export async function getCart(): Promise<ShoppingCart | null> {
+  const localCardId = (await cookies()).get("localCartId")?.value;
+  const cart = localCardId
+    ? await prisma.cart.findUnique({
+        where: { id: localCardId },
+        include: { items: { include: { product: true } } },
+      })
+    : null;
+
+  if (!cart) {
+    return null;
+  }
+
+  return {
+    ...cart,
+    size: cart.items.reduce((acc, item) => acc + item.quantity, 0),
+    subtotal: cart.items.reduce(
+      (acc, item) => acc + item.quantity * item.product.price,
+      0,
+    ),
+  };
+}
+
+export async function createCart(): Promise<ShoppingCart> {
+  const newCart = await prisma.cart.create({
+    data: {},
+  });
+
+  //todo: needs encryption + secure settings in real production app
+  (await cookies()).set("localCardId", newCart.id);
+
+  return {
+    ...newCart,
+    items: [],
+    size: 0,
+    subtotal: 0,
+  };
+}
